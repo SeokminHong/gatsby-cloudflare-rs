6eba63c0c11767fdf46557112db2c10608e8ad63
Seokmin Hong <ghdtjrals240@naver.com>
2021-09-16 00:07:33 +0900

    Remove SSR provider

--- www/gatsby-ssr.js
+++
@@ -1,6 +0,0 @@
-const React = require('react');
-const { PageProvider } = require('./src/contexts/PageContext');
-
-exports.wrapRootElement = ({ element, props }) => (
-  <PageProvider {...props}>{element}</PageProvider>
-);
